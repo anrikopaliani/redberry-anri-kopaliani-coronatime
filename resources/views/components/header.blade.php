93f6f5eeb1b902cf649eac6f3c38f9cd27521452
anrikopaliani <anrikopa@gmail.com>
2023-04-16 15:45:12 +0400

    feat:language switcher

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,3 +1,4 @@
 <div>
+    @include('partials.langauge_switcher')
     <img src="{{ URL::asset('images/coronatime.png') }}" alt="">
 </div>
--- resources/views/partials/langauge_switcher.blade.php
+++ resources/views/partials/langauge_switcher.blade.php
@@ -1,3 +1,7 @@
-<div>
-    <a href="/language/ka">dsadas</a>
+<div class="absolute flex items-center  left-4">
+    @if (app()->getLocale() == 'en')
+        <a href="/language/ka">ka</a>
+    @else
+        <a href="/language/en">{{ __('en') }}</a>
+    @endif
 </div>
