f3aedcc4e0522870df461561b867925647c4b70a
anrikopaliani <anrikopa@gmail.com>
2023-04-16 15:54:19 +0400

    style:language switcher position

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,4 +1,4 @@
-<div>
+<header class="flex items-center">
     @include('partials.langauge_switcher')
     <img src="{{ URL::asset('images/coronatime.png') }}" alt="">
-</div>
+</header>
--- resources/views/partials/langauge_switcher.blade.php
+++ resources/views/partials/langauge_switcher.blade.php
@@ -1,4 +1,4 @@
-<div class="absolute flex items-center  left-4">
+<div class="absolute md:left-4 left-3/4">
     @if (app()->getLocale() == 'en')
         <a href="/language/ka">ka</a>
     @else
