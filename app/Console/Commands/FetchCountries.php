d9070c751a72203f3317a4f8d3d78f991694484a
anrikopaliani <anrikopa@gmail.com>
2023-05-01 21:14:29 +0400

    chore: remove comments

--- app/Console/Commands/FetchCountries.php
+++ app/Console/Commands/FetchCountries.php
@@ -29,25 +29,6 @@ class FetchCountries extends Command
 	public function handle()
 	{
 		$countries = json_decode(Http::connectTimeout(100)->get('https://devtest.ge/countries')->body());
-		// foreach ($countries as $country) {
-		// 	Country::updateOrCreate(
-		// 		['code' => $country->code],
-		// 		['name' => json_encode($country->name)]
-		// 	);
-
-		// 	$stats = json_decode(Http::connectTimeout(100)->post('https://devtest.ge/get-country-statistics', [
-		// 		'code' => $country->code,
-		// 	]));
-
-		// 	CountryStatistic::updateOrCreate(
-		// 		['code'       => $stats->code],
-		// 		['country'    => $stats->country,
-		// 			'confirmed'  => $stats->confirmed,
-		// 			'critical'   => $stats->critical,
-		// 			'deaths'     => $stats->deaths,
-		// 			'recovered'  => $stats->recovered],
-		// 	);
-		// }
 
 		foreach ($countries as $country) {
 			$countryExists = Country::where('code', $country->code)->first();
