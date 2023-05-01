f832a3303b7bf193750734300372b95430eaa15e
anrikopaliani <anrikopa@gmail.com>
2023-05-01 23:13:33 +0400

    refactor: refactor localization

--- resources/views/login/login-form.blade.php
+++ resources/views/login/login-form.blade.php
@@ -4,25 +4,26 @@
     <p class="pb-16 pt-4 opacity-50">{{ __('Please enter your details') }}</p>
     <form class="[&>*:nth-child(2)]:pt-6   md:w-3/4 w-full " action="{{ route('login.post') }}" method="POST">
         @csrf
-        <x-input label="{{ __('Username') }}" name="username" placeholder="{{ __('Enter unique username or email') }}" />
-        <x-input label="{{ __('Password') }}" name="password" type="password"
-            placeholder="{{ __('Fill in password') }}" />
+        <x-input label="{{ __('messages.Username') }}" name="username"
+            placeholder="{{ __('messages.Enter unique username or email') }}" />
+        <x-input label="{{ __('messages.Password') }}" name="password" type="password"
+            placeholder="{{ __('messages.Fill in password') }}" />
         <div class=" w-full flex justify-between py-6 items-center">
             <div>
                 <input type="checkbox" class="focus:accent-form-btn-color active:accent-form-btn-colo text-white"
                     name="remember_me" value="1" id="remember_me">
                 <label for="remember_me"
-                    class=" font-semibold  md:text-base text-xs">{{ __('Remember this device') }}</label>
+                    class=" font-semibold  md:text-base text-xs">{{ __('messages.Remember this device') }}</label>
             </div>
             <a href="{{ route('password.request') }}"
-                class="text-brand-primary font-semibold  md:text-base text-xs">{{ __('Forgot password') }}
+                class="text-brand-primary font-semibold  md:text-base text-xs">{{ __('messages.Forgot password') }}
                 ?</a>
         </div>
         <button type="submit"
             class="bg-form-btn-color mx-auto text-white font-black w-full py-5 rounded-lg">{{ __('LOG IN') }}</button>
     </form>
     <p class="text-center pt-4 md:w-3/4">
-        <span class="opacity-50">{{ __('Don’t have an account') }}?</span>
-        <a href="{{ route('register.get') }}" class="font-bold">{{ __('Sign up for free') }}</a>
+        <span class="opacity-50">{{ __('messages.Don’t have an account') }}?</span>
+        <a href="{{ route('register.get') }}" class="font-bold">{{ __('messages.Sign up for free') }}</a>
     </p>
 </x-form-layout>
--- resources/views/partials/langauge_switcher.blade.php
+++ resources/views/partials/langauge_switcher.blade.php
@@ -1,11 +1,11 @@
 <div x-data="{ show: false }" class="pr-11 md:pr-0">
     <button class="flex justify-between" @click="show = !show">
-        {{ app()->getLocale() == 'en' ? 'English' : 'Georgian' }}
+        {{ app()->getLocale() == 'en' ? 'English' : 'ქართული' }}
         <img width="9.6" height="4.8" class="ml-2"
             src="https://icons.veryicon.com/png/o/miscellaneous/massager/drop-down-arrow-3.png" alt="">
     </button>
     <div x-show="show" class="absolute">
         <a
-            href="/language/{{ app()->getLocale() == 'en' ? 'ka' : 'en' }}">{{ app()->getLocale() == 'en' ? 'Georgian' : 'English' }}</a>
+            href="/language/{{ app()->getLocale() == 'en' ? 'ka' : 'en' }}">{{ app()->getLocale() == 'en' ? 'ქართული' : 'English' }}</a>
     </div>
 </div>
