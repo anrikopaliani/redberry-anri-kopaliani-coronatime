1b3fb3d1805db56e7d46f094b46116ff7229c10c
anrikopaliani <anrikopa@gmail.com>
2023-05-04 02:57:31 +0400

    feat:make county names translatable

--- app/Models/Country.php
+++ app/Models/Country.php
@@ -4,12 +4,13 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Spatie\Translatable\HasTranslations;
 
 class Country extends Model
 {
-	use HasFactory;
+	use HasFactory, HasTranslations;
 
 	protected $guarded = [];
 
-	public $translatable = ['code'];
+	public $translatable = ['name'];
 }
