d04c1399efbba2fba98cd54ae20cadb491149392
anrikopaliani <anrikopa@gmail.com>
2023-05-18 11:52:38 +0400

    fix:font size on worldwide page responsive

--- resources/views/dashboard/worldwide.blade.php
+++ resources/views/dashboard/worldwide.blade.php
@@ -11,21 +11,21 @@
             <div class="h-64 col-span-6 md:col-span-2 bg-brand-primary bg-opacity-0.08 rounded-2xl">
                 <div class="h-full flex flex-col items-center justify-center">
                     <img class="opacity-100 pb-6" src="{{ URL::asset('images/new-cases-logo.png') }}" alt="">
-                    <p class="text-base md:text-xl leading-10 pb-4">{{ __('messages.New cases') }}</p>
+                    <p class="text-sm md:text-xl leading-10 pb-4">{{ __('messages.New cases') }}</p>
                     <p class="text-brand-primary text-2xl md:text-4xl font-black">{{ number_format($newCases) }}</p>
                 </div>
             </div>
             <div class="h-64 col-span-3 md:col-span-2 bg-form-btn-color bg-opacity-0.08 rounded-2xl">
                 <div class="h-full flex flex-col items-center justify-center">
                     <img class="opacity-100 pb-6" src="{{ URL::asset('images/recovered-logo.png') }}" alt="">
-                    <p class="text-base md:text-xl leading-10 pb-4">{{ __('messages.Recovered') }}</p>
+                    <p class="text-sm md:text-xl leading-10 pb-4">{{ __('messages.Recovered') }}</p>
                     <p class="text-brand-primary text-2xl md:text-4xl font-black">{{ number_format($recovered) }}</p>
                 </div>
             </div>
             <div class="h-64 col-span-3 md:col-span-2 bg-brand-tertiary  bg-opacity-0.08  rounded-2xl">
                 <div class="h-full flex flex-col items-center justify-center">
                     <img class="opacity-100 pb-6" src="{{ URL::asset('images/deaths-logo.png') }}" alt="">
-                    <p class="text-base md:text-xl leading-10 pb-4">{{ __('messages.Deaths') }}</p>
+                    <p class="text-sm md:text-xl leading-10 pb-4">{{ __('messages.Deaths') }}</p>
                     <p class="text-brand-primary text-2xl md:text-4xl font-black">{{ number_format($deaths) }}</p>
                 </div>
             </div>
