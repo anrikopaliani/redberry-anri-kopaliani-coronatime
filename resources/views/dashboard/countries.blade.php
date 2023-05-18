24d9d00aa677fdd056a2d7c3f56dd8bc17b7f6a9
anrikopaliani <anrikopa@gmail.com>
2023-05-18 11:43:07 +0400

    fix:horizontal scroll from "dashboard/countries"

--- resources/views/dashboard/countries.blade.php
+++ resources/views/dashboard/countries.blade.php
@@ -14,16 +14,18 @@
                 placeholder="Search by country" value="{{ old('search') }}" type="text">
         </form>
     </div>
-    <div class="{{ app()->getLocale() == 'ka' ? 'w-650' : '' }} md:w-full bg-table-color rounded-t-lg break-all">
+    <div class="md:w-full bg-table-color rounded-t-lg break-all">
         <div class="flex h-14 items-center ">
             <div class="pl-2 flex items-center relative h-full  text-sm md:text-base  md:pl-10 w-1/4 md:w-1/5">
-                @if (!request('location'))
-                    <a href="{!! route('countries-list', ['location=desc', 'search' => request('search')]) !!}">{{ __('messages.Location') }}</a>
-                @elseif (request('location') === 'asc')
-                    <a href="{!! route('countries-list', ['location=desc', 'search' => request('search')]) !!}">{{ __('messages.Location') }}</a>
-                @elseif (request('location') === 'desc')
-                    <a href="{!! route('countries-list', ['location=asc', 'search' => request('search')]) !!}">{{ __('messages.Location') }}</a>
-                @endif
+                <div class=" {{ app()->getLocale() === 'ka' ? 'w-4/5' : '' }}">
+                    @if (!request('location'))
+                        <a href="{!! route('countries-list', ['location=desc', 'search' => request('search')]) !!}">{{ __('messages.Location') }}</a>
+                    @elseif (request('location') === 'asc')
+                        <a href="{!! route('countries-list', ['location=desc', 'search' => request('search')]) !!}">{{ __('messages.Location') }}</a>
+                    @elseif (request('location') === 'desc')
+                        <a href="{!! route('countries-list', ['location=asc', 'search' => request('search')]) !!}">{{ __('messages.Location') }}</a>
+                    @endif
+                </div>
                 <div class="pl-2 flex flex-col ">
                     @if (request('location') === 'asc')
                         <a class="pb-1" href="{{ route('countries-list') }}">
@@ -46,13 +48,15 @@
                 </div>
             </div>
             <div class="w-1/4 flex  items-center relative h-full  text-sm md:text-base  md:w-1/5">
-                @if (!request('cases'))
-                    <a href="{!! route('countries-list', ['cases=desc', 'search' => request('search')]) !!}">{{ __('messages.New cases') }}</a>
-                @elseif (request('cases') === 'asc')
-                    <a href="{!! route('countries-list', ['cases=desc', 'search' => request('search')]) !!}">{{ __('messages.New cases') }}</a>
-                @elseif (request('cases') === 'desc')
-                    <a href="{!! route('countries-list', ['cases=asc', 'search' => request('search')]) !!}">{{ __('messages.New cases') }}</a>
-                @endif
+                <div class=" {{ app()->getLocale() === 'ka' ? 'w-4/5' : '' }}">
+                    @if (!request('cases'))
+                        <a href="{!! route('countries-list', ['cases=desc', 'search' => request('search')]) !!}">{{ __('messages.New cases') }}</a>
+                    @elseif (request('cases') === 'asc')
+                        <a href="{!! route('countries-list', ['cases=desc', 'search' => request('search')]) !!}">{{ __('messages.New cases') }}</a>
+                    @elseif (request('cases') === 'desc')
+                        <a href="{!! route('countries-list', ['cases=asc', 'search' => request('search')]) !!}">{{ __('messages.New cases') }}</a>
+                    @endif
+                </div>
                 <div class="pl-2 flex flex-col ">
                     @if (request('cases') === 'asc')
                         <a class="pb-1" href="{{ route('countries-list') }}">
@@ -76,13 +80,15 @@
                 </div>
             </div>
             <div class="w-1/4 flex  items-center h-full relative  text-sm md:text-base  md:w-1/5">
-                @if (!request('deaths'))
-                    <a href="{!! route('countries-list', ['deaths=desc', 'search' => request('search')]) !!}"> {{ __('messages.Deaths') }}</a>
-                @elseif (request('deaths') === 'asc')
-                    <a href="{!! route('countries-list', ['deaths=desc', 'search' => request('search')]) !!}"> {{ __('messages.Deaths') }}</a>
-                @elseif (request('deaths') === 'desc')
-                    <a href="{!! route('countries-list', ['deaths=asc', 'search' => request('search')]) !!}"> {{ __('messages.Deaths') }}</a>
-                @endif
+                <div class=" {{ app()->getLocale() === 'ka' ? 'w-4/5' : '' }}">
+                    @if (!request('deaths'))
+                        <a href="{!! route('countries-list', ['deaths=desc', 'search' => request('search')]) !!}"> {{ __('messages.Deaths') }}</a>
+                    @elseif (request('deaths') === 'asc')
+                        <a href="{!! route('countries-list', ['deaths=desc', 'search' => request('search')]) !!}"> {{ __('messages.Deaths') }}</a>
+                    @elseif (request('deaths') === 'desc')
+                        <a href="{!! route('countries-list', ['deaths=asc', 'search' => request('search')]) !!}"> {{ __('messages.Deaths') }}</a>
+                    @endif
+                </div>
                 <div class="pl-2 flex flex-col ">
                     @if (request('deaths') === 'asc')
                         <a class="pb-1" href="{{ route('countries-list') }}">
@@ -105,13 +111,15 @@
                 </div>
             </div>
             <div class="w-1/4 flex  items-center h-full relative  text-sm md:text-base  md:w-1/5">
-                @if (!request('recovered'))
-                    <a href="{!! route('countries-list', ['recovered=desc', 'search' => request('search')]) !!}">{{ __('messages.recovered') }}</a>
-                @elseif (request('recovered') === 'asc')
-                    <a href="{!! route('countries-list', ['recovered=desc', 'search' => request('search')]) !!}">{{ __('messages.recovered') }}</a>
-                @elseif (request('recovered') === 'desc')
-                    <a href="{!! route('countries-list', ['recovered=asc', 'search' => request('search')]) !!}">{{ __('messages.recovered') }}</a>
-                @endif
+                <div class=" {{ app()->getLocale() === 'ka' ? 'w-4/5' : '' }}">
+                    @if (!request('recovered'))
+                        <a href="{!! route('countries-list', ['recovered=desc', 'search' => request('search')]) !!}">{{ __('messages.recovered') }}</a>
+                    @elseif (request('recovered') === 'asc')
+                        <a href="{!! route('countries-list', ['recovered=desc', 'search' => request('search')]) !!}">{{ __('messages.recovered') }}</a>
+                    @elseif (request('recovered') === 'desc')
+                        <a href="{!! route('countries-list', ['recovered=asc', 'search' => request('search')]) !!}">{{ __('messages.recovered') }}</a>
+                    @endif
+                </div>
                 <div class="pl-2 flex flex-col ">
                     @if (request('recovered') === 'asc')
                         <a class="pb-1" href="{{ route('countries-list') }}">
